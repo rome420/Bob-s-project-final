171bf1058d6c01c3c409fc0af40ffad4768dedbb
StudentOfSteffen <mohamedamiin123@gmail.com>
2023-12-03 17:27:42 +0100

    I made 6 instance variables and getters

--- SEP/src/project.java
+++ SEP/src/project.java
@@ -4,8 +4,51 @@ public class project
   private MyDate startDate;
   private MyDate endDate;
   private MyDate expectedDuration;
+  private double estimatedPrice;
+  private String projectStatus;
 
-  private double EstimatedPrice;
-  private String ProjectStatus;
+  public Project(String projectType, MyDate startDate, MyDate endDate, MyDate expectedDuration, double estimatedPrice, String projectStatus)
+  {
+    this.projectType = projectType;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.expectedDuration = expectedDuration;
+    this.estimatedPrice = estimatedPrice;
+    this.projectStatus = projectStatus;
+  }
+
+  // Getter methods
+  public String getProjectType()
+  {
+    return projectType;
+  }
+
+  public MyDate getStartDate()
+  {
+    return startDate;
+  }
+
+  public MyDate getEndDate()
+  {
+    return endDate;
+  }
+
+  public MyDate getExpectedDuration()
+  {
+    return expectedDuration;
+  }
+
+  public double getEstimatedPrice()
+  {
+    return estimatedPrice;
+  }
+
+  public String getProjectStatus()
+  {
+    return projectStatus;
+  }
+
 }
+
+
 
