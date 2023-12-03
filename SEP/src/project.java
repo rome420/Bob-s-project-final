c511c7edd9198a47eae0b654f421a913cc5147f4
StudentOfSteffen <mohamedamiin123@gmail.com>
2023-12-03 17:00:39 +0100

    I made 6 instance variables

--- SEP/src/project.java
+++ SEP/src/project.java
@@ -1,3 +1,11 @@
 public class project
 {
+  private String projectType;
+  private MyDate startDate;
+  private MyDate endDate;
+  private MyDate expectedDuration;
+
+  private double EstimatedPrice;
+  private String ProjectStatus;
 }
+
