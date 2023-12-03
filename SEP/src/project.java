410c13c1872e39517115bcdb7d50684f412bc7cf
StudentOfSteffen <mohamedamiin123@gmail.com>
2023-12-03 21:00:08 +0100

    all instance variables + their setters and getter are here

--- SEP/src/project.java
+++ SEP/src/project.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Project {
+public abstract class project
+{
   private String projectType;
   private myDate startDate;
   private myDate endDate;
@@ -10,10 +11,10 @@ public abstract class Project {
   private String projectStatus;
   private boolean isFinished;
   private double projectId;
-
   private double manHoursUsed;
 
-  public Project(String projectType, myDate startDate, myDate endDate, myDate expectedDuration, double estimatedPrice, String projectStatus) {
+  public project(String projectType, myDate startDate, myDate endDate, myDate expectedDuration, double estimatedPrice, String projectStatus,double projectId,double manHoursUsed)
+  {
     this.projectType = projectType;
     this.startDate = startDate;
     this.endDate = endDate;
@@ -22,73 +23,99 @@ public abstract class Project {
     this.projectStatus = projectStatus;
     // Assuming the default value for isFinished is false when a project is created
     this.isFinished = false;
+    this.projectId= projectId;
+    this.manHoursUsed=0;
   }
 
   // Getter methods...
-  public String getProjectType() {
+  public String getProjectType()
+  {
     return projectType;
   }
 
-  public myDate getStartDate() {
+  public myDate getStartDate()
+  {
     return startDate;
   }
 
-  public myDate getEndDate() {
+  public myDate getEndDate()
+  {
     return endDate;
   }
 
-  public myDate getExpectedDuration() {
+  public myDate getExpectedDuration()
+  {
     return expectedDuration;
   }
 
-  public double getEstimatedPrice() {
+  public double getEstimatedPrice()
+  {
     return estimatedPrice;
   }
 
-  public String getProjectStatus() {
+  public String getProjectStatus()
+  {
     return projectStatus;
   }
 
-  public Boolean getFinishedProjects() {
+  public Boolean getFinishedProjects()
+  {
     return isFinished;
   }
+  public double getProjectId(){
+    return projectId;
+}
+
+public double getManHoursUsed(){
+    return manHoursUsed;
+}
 
   // Setter methods...
-  public void setProjectType(String projectType) {
+  public void setProjectType(String projectType)
+  {
     this.projectType = projectType;
   }
 
-  public void setStartDate(myDate startDate) {
+  public void setStartDate(myDate startDate)
+  {
     this.startDate = startDate;
   }
 
-  public void setEndDate(myDate endDate) {
+
+
+  public void setEndDate(myDate endDate)
+  {
     this.endDate = endDate;
   }
 
-  public void setExpectedDuration(myDate expectedDuration) {
+  public void setExpectedDuration(myDate expectedDuration)
+  {
     this.expectedDuration = expectedDuration;
   }
 
-  public void setEstimatedPrice(double estimatedPrice) {
+  public void setEstimatedPrice(double estimatedPrice)
+  {
     this.estimatedPrice = estimatedPrice;
   }
 
-  public void setProjectStatus(String projectStatus) {
+  public void setProjectStatus(String projectStatus)
+  {
     this.projectStatus = projectStatus;
   }
 
-  public void setFinishedProjects(boolean isFinished) {
+  public void setFinishedProjects(boolean isFinished)
+  {
     this.isFinished = isFinished;
   }
 
-  // Abstract method to assign a team member
-  public abstract int assignTeamMember(int teamMemberNumber);
+  public void setProjectId(double projectId){
+    this.projectId=projectId;
+  }
 
-  // Additional method to get the list of team members
-  public abstract List<Integer> getTeamMembers();
+  public void setManHoursUsed(double manHoursUsed){
+    this.manHoursUsed=manHoursUsed;
 }
 
-
+}
 
 
