56502ebe5a4165fea2b3eec3ac0f59e1206bf344
StudentOfSteffen <mohamedamiin123@gmail.com>
2023-12-06 11:15:50 +0100

    i created a DefaultCommercialProject method so bob can create a Commercial project

--- SEP/src/ProjectManager.java
+++ SEP/src/ProjectManager.java
@@ -1,73 +1,95 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProjectManager {
+public class ProjectManager
+{
   private List<Project> projects;
   private List<Customer> customers;
 
-  public ProjectManager() {
+  public ProjectManager()
+  {
     projects = new ArrayList<>();
     customers = new ArrayList<>();
   }
 
   // Project-related methods
 
-  public void addProject(Project project, boolean isFinished) {
+  public void addProject(Project project, boolean isFinished)
+  {
     projects.add(project);
     project.setFinishedProjects(isFinished);
   }
 
-  public void removeProject(Project project, boolean isFinished) {
+  public void removeProject(Project project, boolean isFinished)
+  {
     projects.remove(project);
     project.setFinishedProjects(isFinished);
   }
 
-  public List<Project> getAllProjects() {
+  public List<Project> getAllProjects()
+  {
     return projects;
   }
 
-  public Project findProjectById(double projectId) {
-    for (Project project : projects) {
-      if (project.getId() == projectId) {
+  public Project findProjectById(double projectId)
+  {
+    for (Project project : projects)
+    {
+      if (project.getId() == projectId)
+      {
         return project;
       }
     }
     return null;
   }
 
-
-  public void addCustomer(Customer customer) {
+  public void addCustomer(Customer customer)
+  {
     customers.add(customer);
   }
 
-  public void removeCustomer(Customer customer) {
+  public void removeCustomer(Customer customer)
+  {
     customers.remove(customer);
   }
 
-  public List<Customer> getAllCustomers() {
+  public List<Customer> getAllCustomers()
+  {
     return customers;
   }
 
-  public Customer findCustomerByName(String name) {
-    for (Customer customer : customers) {
-      if (customer.getName().equals(name)) {
+  public Customer findCustomerByName(String name)
+  {
+    for (Customer customer : customers)
+    {
+      if (customer.getName().equals(name))
+      {
         return customer;
       }
     }
     return null;
   }
 
-  public List<Project> getProjectsByType(String projectType) {
+  public List<Project> getProjectsByType(String projectType)
+  {
     List<Project> projectsByType = new ArrayList<>();
 
-    for (Project project : projects) {
-      if (projectType.equalsIgnoreCase("Commercial") && project instanceof Commercial) {
+    for (Project project : projects)
+    {
+      if (projectType.equalsIgnoreCase("Commercial") && project instanceof Commercial)
+      {
         projectsByType.add(project);
-      } else if (projectType.equalsIgnoreCase("Industrial") && project instanceof Industrial) {
+      }
+      else if (projectType.equalsIgnoreCase("Industrial") && project instanceof Industrial)
+      {
         projectsByType.add(project);
-      } else if (projectType.equalsIgnoreCase("RoadConstruction") && project instanceof RoadConstruction) {
+      }
+      else if (projectType.equalsIgnoreCase("RoadConstruction") && project instanceof RoadConstruction)
+      {
         projectsByType.add(project);
-      } else if (projectType.equalsIgnoreCase("Residential") && project instanceof Residential) {
+      }
+      else if (projectType.equalsIgnoreCase("Residential") && project instanceof Residential)
+      {
         projectsByType.add(project);
       }
     }
@@ -75,4 +97,35 @@ public class ProjectManager {
     return projectsByType;
   }
 
+  public Commercial createDefaultCommercialProject() {
+    // You can set default values for Commercial projects here
+    Commercial commercial = new Commercial("Commercial", new MyDate(26, 8, 2001), new MyDate(0,0,0000), new MyDate(0),
+        0, "New Build", 0, 0, 0, 1, "Default Building Use");
+
+    // Set specific default values for Commercial
+    double estimatedPrice = 500000; // Set the default estimated price to 500000
+    int durationInMonths = 18; // Set the default duration to 18 months
+
+    // Validate the estimated price and duration
+    if (estimatedPrice >= 500000 && estimatedPrice <= 2000000
+        && durationInMonths >= 12 && durationInMonths <= 24) {
+      commercial.setEstimatedPrice(estimatedPrice);
+      commercial.setDurationInMonths(durationInMonths);
+    } else {
+      commercial.setEstimatedPrice(0);
+      commercial.setDurationInMonths(0); // Use the new method to set duration directly
+    }
+
+    return commercial;
+  }
+
+
+
+
+
 }
+
+
+
+
+
