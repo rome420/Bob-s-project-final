9ba68f94c086719b76a674aa5b314386283c201e
StudentOfSteffen <mohamedamiin123@gmail.com>
2023-12-06 07:11:17 +0100

    added default values for each instance varriable and 1+ instance variable "timeline"

--- SEP/src/Residential.java
+++ SEP/src/Residential.java
@@ -5,16 +5,25 @@ class Residential extends Project {
   private int otherRoomsWithPlumbing;
   private boolean isNewBuild;
 
+  private int timeline;
+
   public Residential(String projectType, MyDate startDate, MyDate endDate, MyDate expectedDuration,
       double estimatedPrice, String projectStatus, double projectId, double manHoursUsed,
-      double squareMeters) {
+      double squareMeters,int numberOfKitchens, int numberOfBathrooms,
+      int otherRoomsWithPlumbing, boolean isNewBuild,int timeline) {
     super(projectType, startDate, endDate, expectedDuration, estimatedPrice, projectStatus, projectId, manHoursUsed);
     this.squareMeters = squareMeters;
+    this.numberOfKitchens = 1;
+    this.numberOfBathrooms = 1;
+    this.otherRoomsWithPlumbing = otherRoomsWithPlumbing;
+    this.isNewBuild = true;
+    this.timeline=9;
+
   }
 
   public double getSquareMeters() {
     return squareMeters;
   }
 
-//
+
 }
