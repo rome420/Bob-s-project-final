aebfa62797ea59ac57f44be78145d762c518bad9
StudentOfSteffen <mohamedamiin123@gmail.com>
2023-12-06 08:45:36 +0100

    New instance variable String facilityType+getter and setters for it also setter for double squareMeters.

---
+++ SEP/src/Industrial.java
@@ -0,0 +1,29 @@
+class Industrial extends Project {
+  private double squareMeters;
+  private String facilityType;
+
+  public Industrial(String projectType, MyDate startDate, MyDate endDate, MyDate expectedDuration,
+      double estimatedPrice, String projectStatus, double projectId, double manHoursUsed,
+      double squareMeters,String facilityType) {
+    super(projectType, startDate, endDate, expectedDuration, estimatedPrice, projectStatus, projectId, manHoursUsed);
+    this.squareMeters = squareMeters;
+    this.facilityType=facilityType;
+  }
+
+  public double getSquareMeters() {
+    return squareMeters;
+  }
+
+  public String getFacilityType() {
+    return facilityType;
+  }
+
+  public void setSquareMeters(double squareMeters) {
+    this.squareMeters = squareMeters;
+  }
+
+  public void setFacilityType(String facilityType) {
+    this.facilityType = facilityType;
+  }
+
+}
